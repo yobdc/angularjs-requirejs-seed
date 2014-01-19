c164a908c84f6cac2a5a7a78fed69d4d02a586a9
yobdc pu <yobdc.101@gmail.com>
2014-01-19 16:57:47 +0800

    为protractor添加简单test case

--- test/e2e/example_spec.js
+++ test/e2e/example_spec.js
@@ -1,12 +1,20 @@
 describe('angularjs homepage', function() {
- var ptor= protractor.getInstance();
+  var ptor = protractor.getInstance();
+  var targetUrl = 'http://localhost:8000/#/supplier/create';
 
   beforeEach(function() {
-    ptor.driver.get('http://localhost:8000/#/supplier/create'); // Just use the plain old webdriver here, it won't complain if Angular isn't loaded yet.
+    ptor.ignoreSynchronization = true;
+    ptor.get(targetUrl); // Just use the plain old webdriver here, it won't complain if Angular isn't loaded yet.
   });
 
-  it('list, no filter', function() {
-    console.log(ptor);
-    // var selectOption = ptor.findElement(ptor.By.input("selectOption"));
+  it('first e2e test', function() {
+    ptor.driver.getCurrentUrl().then(function(url) {
+      console.log(url);
+      expect(url).toEqual(targetUrl);
+    });
+    ptor.driver.getTitle().then(function(title) {
+      console.log(title);
+      expect(title).not.toBe(null);
+    });
   });
 });
